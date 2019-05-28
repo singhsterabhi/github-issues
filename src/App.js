aadf43892e38a7a86709c6119265324657dd9fb6
singhsterabhi <singhsterabhi@gmail.com>
2019-05-29 01:49:38 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    repo: "",
+    openIssues: ""
+  };
+  async componentDidMount() {
+    const url = "https://github.com/ncs-jss/HTTP_200";
+    const urlp = url.split("/");
+    console.log(urlp);
+    let day = new Date(2016, 5, 17);
+    let dayWrapper = moment(day).toISOString();
+    console.log(dayWrapper);
+
+    // const reqUrl = `https://api.github.com/repos/${urlp[3]}/${
+    //   urlp[4]
+    // }/issues?since=${dayWrapper}`;
+
+    // const reqUrl = `https://api.github.com/repos/${urlp[3]}/${urlp[4]}`;
+
+    const reqUrl = `https://api.github.com/search/issues?q=repo:${urlp[3]}/${
+      urlp[4]
+    }+type:issue+state:open+created_at>=${dayWrapper}`;
+
+    console.log(reqUrl);
+    const data = await fetch(reqUrl).then(d => d.json());
+    console.log(data);
+    console.log(data["open_issues_count"]);
+    this.setState({
+      repo: data["full_name"],
+      openIssues: data["open_issues_count"]
+    });
+  }
+  render() {
+    return (
+      <>
+        <p>hello</p>
+        <p>Repo : {this.state.repo}</p>
+        <p>open_issues_count : {this.state.openIssues}</p>
+      </>
+    );
+  }
 }
 
 export default App;
