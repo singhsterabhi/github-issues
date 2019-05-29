828900d0d42baa283e801db5218173695c389d37
Abhishek <singhsterabhi@gmail.com>
2019-05-29 15:22:56 +0530

    issue count search

--- src/App.js
+++ src/App.js
@@ -1,45 +1,158 @@
 import React, { Component } from "react";
 import moment from "moment";
+import "./App.scss";
 
 class App extends Component {
   state = {
+    searching: false,
+    repository: "",
     repo: "",
-    openIssues: ""
+    openIssues: "",
+    pastOneDay: "",
+    betOneToSeven: "",
+    beforeSeven: ""
   };
-  async componentDidMount() {
-    const url = "https://github.com/ncs-jss/HTTP_200";
+
+  searchIssues = async () => {
+    const url = this.state.repository;
     const urlp = url.split("/");
     console.log(urlp);
-    let day = new Date(2016, 5, 17);
-    let dayWrapper = moment(day).toISOString();
-    console.log(dayWrapper);
 
-    // const reqUrl = `https://api.github.com/repos/${urlp[3]}/${
-    //   urlp[4]
-    // }/issues?since=${dayWrapper}`;
+    let reqUrl = `https://api.github.com/search/issues?q=repo:${urlp[3]}/${
+      urlp[4]
+    }+type:issue+state:open`;
 
-    const reqUrl = `https://api.github.com/repos/${urlp[3]}/${urlp[4]}`;
+    let data = await fetch(reqUrl)
+      .then(d => d.json())
+      .catch(e => {
+        console.log(e);
+        alert("some error");
+        return;
+      });
 
-    // const reqUrl = `https://api.github.com/search/issues?q=repo:${urlp[3]}/${
-    //   urlp[4]
-    // }+type:issue+state:open+created_at>=${dayWrapper}`;
+    const openIssues = data["total_count"];
 
-    console.log(reqUrl);
-    const data = await fetch(reqUrl).then(d => d.json());
-    console.log(data);
-    console.log(data["open_issues_count"]);
+    let today = new Date();
+    let day = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+    let dayWrapper = moment(day)
+      .toISOString()
+      .split("T")[0];
+
+    reqUrl = `https://api.github.com/search/issues?q=repo:${urlp[3]}/${
+      urlp[4]
+    }+created:>=${dayWrapper}+type:issue+state:open`;
+
+    data = await fetch(reqUrl)
+      .then(d => d.json())
+      .catch(e => {
+        console.log(e);
+        alert("some error");
+        return;
+      });
+
+    const pastOneDay = data["total_count"];
+
+    day = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+    dayWrapper = moment(day)
+      .toISOString()
+      .split("T")[0];
+    reqUrl = `https://api.github.com/search/issues?q=repo:${urlp[3]}/${
+      urlp[4]
+    }+created:>=${dayWrapper}+type:issue+state:open`;
+
+    data = await fetch(reqUrl)
+      .then(d => d.json())
+      .catch(e => {
+        console.log(e);
+        alert("some error");
+        return;
+      });
+
+    if (!data["total_count"]) {
+      console.log("Some error");
+      alert("Some error");
+      return;
+    }
+
+    const betOneToSeven = data["total_count"] - pastOneDay;
+    const beforeSeven = openIssues - (betOneToSeven + pastOneDay);
+
     this.setState({
-      repo: data["full_name"],
-      openIssues: data["open_issues_count"]
+      repo: `${urlp[3]} - ${urlp[4]}`,
+      openIssues,
+      pastOneDay,
+      betOneToSeven,
+      beforeSeven
     });
-  }
+  };
+
+  handleSearch = e => {
+    e.preventDefault();
+    if (this.state.repository === "") {
+      alert("Enter a repo url");
+      return;
+    }
+    this.setState({
+      searching: true,
+      repo: "",
+      openIssues: "",
+      pastOneDay: "",
+      betOneToSeven: "",
+      beforeSeven: ""
+    });
+    this.searchIssues();
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     return (
-      <>
-        <p>hello</p>
-        <p>Repo : {this.state.repo}</p>
-        <p>open_issues_count : {this.state.openIssues}</p>
-      </>
+      <div className="app">
+        <form onSubmit={this.handleSearch}>
+          <input
+            type="text"
+            name="repository"
+            value={this.state.repository}
+            onChange={this.handleChange}
+            placeholder="https://github.com/reactjs/reactjs.org"
+          />
+          <br />
+          <button type="submit">Search</button>
+        </form>
+
+        {this.state.repo === "" ? (
+          ""
+        ) : (
+          <table>
+            <tbody>
+              <tr>
+                <td>Repo</td>
+                <td>{this.state.repo}</td>
+              </tr>
+              <tr>
+                <td>Total Open issues</td>
+                <td>{this.state.openIssues}</td>
+              </tr>
+              <tr>
+                <td>No. of open issues opened in past 24 hrs</td>
+                <td>{this.state.pastOneDay}</td>
+              </tr>
+              <tr>
+                <td>No. of open issues opened in past 1-7 days</td>
+                <td>{this.state.betOneToSeven}</td>
+              </tr>
+              <tr>
+                <td>No. of open issues opened more than 7 days ago</td>
+                <td>{this.state.beforeSeven}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+      </div>
     );
   }
 }
